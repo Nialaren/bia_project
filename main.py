10347565c33bacf41104a42e1753061caf6bd45a
Radka Ceplakova <r.ceplakova@gmail.com>
2015-11-23 17:08:20 +0100

    + reload function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-import testFunction as TF
+import protokol_2 as TF
 
 
 
@@ -30,14 +30,13 @@ class Form(QtGui.QWidget):
         mainLabel.setAlignment(QtCore.Qt.AlignTop)
 
         functionSelector = QtGui.QComboBox()
-        functionSelector.addItems(['firstDeJong', 'secondDeJong', 'thirdDeJong'])
+        functionSelector.addItems(['First De Jong', 'Rosenbrocks Saddle', 'Third De Jong', 'Forth De Jong',
+                     'Rastrigin', 'Schewefel', 'Griewangkova', 'Sine Envelope', 'Sine Wave'])
         functionSelector.connect(functionSelector, QtCore.SIGNAL("currentIndexChanged(int)"), self, QtCore.SLOT("updateUI(int)"))
-        btn = QtGui.QPushButton('change', self)
 
         # Add all widgets into layout
         cpLayout.addWidget(mainLabel)
         cpLayout.addWidget(functionSelector)
-        cpLayout.addWidget(btn)
 
 
 
@@ -65,7 +64,7 @@ class Form(QtGui.QWidget):
         Z = TF.firstDeJong(basicPlane)
 
 
-        surface = Axes3D.plot_surface(self.axes, X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+        self.surf = Axes3D.plot_surface(self.axes, X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
         # self.axes.set_zlim(-1, 1000)
         # in the end just add canvas to layout
 
@@ -73,23 +72,26 @@ class Form(QtGui.QWidget):
     @QtCore.pyqtSlot(int)
     def updateUI(self, index):
         # print self.figure.subplotpars.
-        print 'test %s' %(index)
+        # print 'test %s' %(index)
+
+
         #draw data
-        # X = np.arange(-2, 2, 0.25)
-        # #Basic plane
-        # basicPlane = np.meshgrid(X, X)
-        #
-        # X, Y = basicPlane
-        #
-        # # here choose which test plane use
-        # Z = TF.firstDeJong(basicPlane)
-        #
-        # # self.axes.clear()
-        # self.surf.remove()
-        # self.surf = Axes3D.plot_surface(self.axes, X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-        # self.canvas.draw()
+        X = np.arange(-2, 2, 0.25)
+        #Basic plane
+        basicPlane = np.meshgrid(X, X)
 
+        X, Y = basicPlane
 
+        functions = [TF.firstDeJong(basicPlane), TF.rosenbrocksSaddle(basicPlane), TF.thirdDeJong(basicPlane), TF.forthDeJong(basicPlane),
+                     TF.rastrigin(basicPlane), TF.schewefel(basicPlane), TF.griewangkova(basicPlane), TF.sineEnvelope(basicPlane),
+                     TF.sineWave(basicPlane)]
+        # here choose which test plane use
+        Z = functions[index]
+
+        # self.axes.clear()
+        self.surf.remove()
+        self.surf = Axes3D.plot_surface(self.axes, X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+        self.canvas.draw()
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
